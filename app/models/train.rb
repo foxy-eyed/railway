99250fe001acce52314cd1cba7aadc9b34279cab
Anna Selivanova <foxy.eyed@gmail.com>
2016-09-11 19:42:33 +0400

    Add wagons

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,6 +2,19 @@ class Train < ApplicationRecord
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
   belongs_to :route
   has_many :tickets
+  has_many :wagons
 
   validates :number, presence: true
+
+  def capacity_by_categories
+    capacity = {}
+    Wagon::CATEGORIES.each_key { |key| capacity[key] = { wagons: 0, top_places: 0, bottom_places: 0 } }
+    wagons.each do |wagon|
+      category = wagon.category.to_sym
+      capacity[category][:wagons] += 1
+      capacity[category][:top_places] += wagon.top_places
+      capacity[category][:bottom_places] += wagon.bottom_places
+    end
+    capacity
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905192900) do
+ActiveRecord::Schema.define(version: 20160911094849) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -60,6 +60,16 @@ ActiveRecord::Schema.define(version: 20160905192900) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "wagons", force: :cascade do |t|
+    t.string   "category"
+    t.integer  "top_places"
+    t.integer  "bottom_places"
+    t.integer  "train_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["train_id"], name: "index_wagons_on_train_id"
   end
 
 end
