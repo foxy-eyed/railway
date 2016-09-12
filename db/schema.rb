b8fd74464d3ed538f830fa7b9f6d3f777b661d96
Selivanova Anna <foxy.eyed@gmail.com>
2016-09-12 15:55:59 +0400

    Add default value to places count in wagon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911094849) do
+ActiveRecord::Schema.define(version: 20160912114554) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -64,11 +64,11 @@ ActiveRecord::Schema.define(version: 20160911094849) do
 
   create_table "wagons", force: :cascade do |t|
     t.string   "category"
-    t.integer  "top_places"
-    t.integer  "bottom_places"
+    t.integer  "top_places",    default: 0
+    t.integer  "bottom_places", default: 0
     t.integer  "train_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.index ["train_id"], name: "index_wagons_on_train_id"
   end
 
