97acc48a765c45b8bb87d1171817cc6c19699ad2
Anna Selivanova <foxy.eyed@gmail.com>
2016-09-18 18:38:47 +0400

    Make wagons nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
     patch :update_position, on: :member
   end
   resources :routes
-  resources :trains
-  resources :wagons
+  resources :trains do
+    resources :wagons, shallow: true
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
